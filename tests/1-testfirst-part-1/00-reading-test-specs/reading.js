08d67b2e6ff3774a32f524559a94a48b3f6d33ca
im-mahesh1 <maheshchaudhary2057@gmail.com>
2025-07-19 21:12:26 +0545

    task1 is completed

--- tests/1-testfirst-part-1/00-reading-test-specs/reading.js
+++ tests/1-testfirst-part-1/00-reading-test-specs/reading.js
@@ -1,7 +1,7 @@
 /* eslint-disable no-unused-vars */
 function hello(name) {
-  if (typeof name === 'undefined') {
-    return 'Hello!';
+  if (typeof name === "undefined") {
+    return "Hello!";
   } else {
     return `Hello, ${name}!`;
   }
