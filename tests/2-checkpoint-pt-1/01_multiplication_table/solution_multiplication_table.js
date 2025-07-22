f383eb49f107b827fce9ca7bdd47c45f9f8e7a02
im-mahesh1 <maheshchaudhary2057@gmail.com>
2025-07-22 10:20:43 +0545

    multiplication table is done

--- tests/2-checkpoint-pt-1/01_multiplication_table/solution_multiplication_table.js
+++ tests/2-checkpoint-pt-1/01_multiplication_table/solution_multiplication_table.js
@@ -1,1 +1,11 @@
 /* eslint-disable no-unused-vars */
+const multiplicationTable = (row, column) => {
+    result = [];
+    for (let i = 0; i < row; i++){
+        result[i] = []
+        for (let j = 0; j < column; j++){
+            result[i][j] =(j+1)*(i+1)
+        }
+    }
+    return result;
+}
