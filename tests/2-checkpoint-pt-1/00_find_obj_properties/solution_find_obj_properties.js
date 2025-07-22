04f26b95bb781f042ad024018eeeca4377c953e3
im-mahesh1 <maheshchaudhary2057@gmail.com>
2025-07-22 10:19:02 +0545

    object properties done

--- tests/2-checkpoint-pt-1/00_find_obj_properties/solution_find_obj_properties.js
+++ tests/2-checkpoint-pt-1/00_find_obj_properties/solution_find_obj_properties.js
@@ -1,1 +1,10 @@
 /* eslint-disable no-unused-vars, no-prototype-builtins */
+const findObjPropsHasOwn = (obj1) => {
+  let keys = [];
+    for (let key in obj1) {
+        if (obj1.hasOwnProperty(key)) {
+            keys.push(key)
+        }
+    }
+    return keys.join(", ");
+};
