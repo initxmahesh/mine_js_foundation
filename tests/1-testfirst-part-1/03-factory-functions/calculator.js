369ff25cf3d21bc1ff4cbd4c7725a986688f05e7
im-mahesh1 <maheshchaudhary2057@gmail.com>
2025-07-22 07:40:38 +0545

    factory functions is done

--- tests/1-testfirst-part-1/03-factory-functions/calculator.js
+++ tests/1-testfirst-part-1/03-factory-functions/calculator.js
@@ -1,1 +1,36 @@
 /* eslint-disable no-unused-vars */
+const createCalculator = () => {
+  return {
+    total: 0,
+    add(num) {
+      this.total += num;
+    },
+    subtract(num) {
+      this.total -= num;
+    },
+    value() {
+      return this.total;
+    },
+    clear() {
+      this.total = 0;
+    },
+  };
+};
+
+const addSquareMethod = (array) => {
+  for (let i = 0; i < array.length; i++) {
+    array[i].square = function () {
+      return this.total * this.total;
+    };
+  }
+  return array;
+};
+
+const createHumanCalculator = () => {
+  let instance = Object.create(createCalculator());
+  instance.__proto__.total = -10;
+  instance.__proto__.clear = function () {
+    this.total = -10;
+  };
+  return instance;
+};
