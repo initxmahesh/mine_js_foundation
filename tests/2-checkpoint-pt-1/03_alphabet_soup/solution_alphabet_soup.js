350fd40a055b9a851307f8d21a776ce2627c76a6
im-mahesh1 <maheshchaudhary2057@gmail.com>
2025-07-23 06:38:41 +0545

    alphabet soup is done

--- tests/2-checkpoint-pt-1/03_alphabet_soup/solution_alphabet_soup.js
+++ tests/2-checkpoint-pt-1/03_alphabet_soup/solution_alphabet_soup.js
@@ -1,1 +1,21 @@
 /* eslint-disable no-unused-vars, no-prototype-builtins */
+/* eslint-disable no-unused-vars, no-prototype-builtins */
+const soup = (phrase, bank) => {
+  let phraseArr = phrase.split("");
+  let isFalse = false;
+
+  if (bank.length < phrase.length) {
+    return false;
+  }
+  for (let i = 0; i < phrase.length; i++) {
+    if (bank.includes(phraseArr[i])) {
+      isFalse = true;
+      continue;
+    } else {
+      isFalse = false;
+      break;
+    }
+  }
+
+  return isFalse;
+};
