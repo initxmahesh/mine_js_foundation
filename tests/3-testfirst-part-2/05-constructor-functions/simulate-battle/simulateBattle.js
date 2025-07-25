e76d35d3845d57752ec031d1ab1387ffaa4d4bf6
im-mahesh1 <maheshchaudhary2057@gmail.com>
2025-07-25 17:11:31 +0545

    05-constructor-functions is completed

--- tests/3-testfirst-part-2/05-constructor-functions/simulate-battle/simulateBattle.js
+++ tests/3-testfirst-part-2/05-constructor-functions/simulate-battle/simulateBattle.js
@@ -1,1 +1,36 @@
 /* eslint-disable no-unused-vars, no-throw-literal*/
+/* eslint-disable no-unused-vars, no-throw-literal*/
+function Pokemon(name, health, attackBonus) {
+  this.name = name;
+  this.health = health;
+  this.attackBonus = attackBonus;
+}
+
+Pokemon.prototype.isDefeated = function () {
+  return this.health <= 0;
+};
+
+Pokemon.prototype.biteAttack = function () {
+  return this.attackBonus + 2;
+};
+
+const simulateBattle = (Pokemon1, Pokemon2, firstAttack) => {
+  let attacker;
+  let defender;
+  if (Pokemon1.name === firstAttack) {
+    attacker = Pokemon1;
+    defender = Pokemon2;
+  } else {
+    attacker = Pokemon2;
+    defender = Pokemon1;
+  }
+
+  while (true) {
+    defender.health -= attacker.biteAttack();
+    if (defender.isDefeated()) {
+      return `${attacker.name} Wins!`;
+    }
+
+    [attacker, defender] = [defender, attacker];
+  }
+};
