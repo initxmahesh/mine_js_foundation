c1939e0a646abfb19bb47f231c735b6d34808b60
im-mahesh1 <maheshchaudhary2057@gmail.com>
2025-07-26 14:58:22 +0545

    function js is completed

--- tests/3-testfirst-part-2/06-functions-and-closure/functions.js
+++ tests/3-testfirst-part-2/06-functions-and-closure/functions.js
@@ -1,1 +1,60 @@
 /* eslint-disable no-unused-vars */
+
+const concatString = function () {
+  let arg = Array.from(arguments);
+  return arg.join("");
+};
+
+const yourFunctionRunner = function () {
+  let result = "";
+  for (let i = 0; i < arguments.length; i++) {
+    result += arguments[i]();
+  }
+  return result;
+};
+
+const makeAdder = function (num) {
+  return function (arg1) {
+    return arg1 + num;
+  };
+};
+
+const once = (func) => {
+  let notCalled = true;
+  return () => {
+    if (notCalled) {
+      notCalled = false;
+      return func();
+    }
+    return "the function has already been called...";
+  };
+};
+
+const createObjectWithClosures = function () {
+  let value = 0;
+  return {
+    oneIncrementer: function () {
+      value += 1;
+    },
+    tensIncrementer: function () {
+      value += 10;
+    },
+    getValue: function () {
+      return value;
+    },
+    setValue: function (newValue) {
+      value = newValue;
+    },
+  };
+};
+
+const dontSpillTheBeans = function (secret) {
+  return {
+    getSecret: function () {
+      return secret;
+    },
+    setSecret: function (newsecret) {
+      secret = newsecret;
+    },
+  };
+};
