d5b30b54af86c73f58ae232066a887eb655306fd
im-mahesh1 <maheshchaudhary2057@gmail.com>
2025-07-27 22:06:16 +0545

    refactor PRN calculator is completed

--- tests/5-testfirst-part-3/08-Classes/00-refactor-RPN-Calculator/solution-refactor-rpn-calculator.js
+++ tests/5-testfirst-part-3/08-Classes/00-refactor-RPN-Calculator/solution-refactor-rpn-calculator.js
@@ -3,3 +3,37 @@
 /*
   Refactor the constructor function below and use ES2015 Class Syntax
 */
+class RPNCalculator {
+  constructor() {
+    this.stack = [];
+  }
+
+  push(num) {
+    this.stack.push(num);
+  }
+
+  operation(compute) {
+    if (this.stack.length < 2) {
+      throw "rpnCalculatorInstance is empty";
+    } else {
+      const a = this.stack.pop();
+      const b = this.stack.pop();
+      this.push(compute(a, b));
+    }
+  }
+  plus() {
+    this.operation((a, b) => a + b);
+  }
+  minus() {
+    this.operation((a, b) => b - a);
+  }
+  times() {
+    this.operation((a, b) => a * b);
+  }
+  divide() {
+    this.operation((a, b) => b / a);
+  }
+  value() {
+    return this.stack[this.stack.length - 1];
+  }
+}
