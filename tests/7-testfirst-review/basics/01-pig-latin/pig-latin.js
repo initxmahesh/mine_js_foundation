da6995dd46027c3807fdbc16b104452ed5741bd5
im-mahesh1 <maheshchaudhary2057@gmail.com>
2025-08-02 08:49:01 +0545

    feat: pig latin is added

--- tests/7-testfirst-review/basics/01-pig-latin/pig-latin.js
+++ tests/7-testfirst-review/basics/01-pig-latin/pig-latin.js
@@ -1,1 +1,24 @@
 /* eslint-disable no-unused-vars */
+function pigify(str) {
+  let arr = str.split(" ");
+  let vowel = "aeiou";
+
+  return arr
+    .reduce((acc, curr) => {
+      let indexOfVowel = 0;
+      for (let i = 0; i < curr.length; i++) {
+        const letter = curr[i];
+        if (vowel.includes(letter)) {
+          if (letter !== "u" || (letter === "u" && curr[i - 1] !== "q")) {
+            indexOfVowel = i;
+            break;
+          }
+        }
+      }
+      const startNoConsonants = curr.slice(indexOfVowel);
+      const consonantsForEnd = curr.slice(0, indexOfVowel);
+      acc = [...acc, `${startNoConsonants}${consonantsForEnd}ay`];
+      return acc;
+    }, [])
+    .join(" ");
+}
