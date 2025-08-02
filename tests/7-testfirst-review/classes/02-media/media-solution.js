3b8bd7b2508253005b2a223e806c21093e352867
im-mahesh1 <maheshchaudhary2057@gmail.com>
2025-08-02 09:18:04 +0545

    feat: media is added

--- tests/7-testfirst-review/classes/02-media/media-solution.js
+++ tests/7-testfirst-review/classes/02-media/media-solution.js
@@ -1,1 +1,57 @@
 /* eslint-disable no-unused-vars */
+class Media {
+  constructor(title, yearOfRelease, ratings = []) {
+    this.title = title;
+    this.yearOfRelease = yearOfRelease;
+    this.ratings = ratings;
+  }
+
+  getTitle() {
+    return this.title;
+  }
+
+  getYearOfRelease() {
+    return this.yearOfRelease;
+  }
+
+  addRating(num) {
+    if (num >= 1 && num <= 100) return this.ratings.push(num);
+    else {
+      return `${num} is not a valid rating, ratings must be 1-100`;
+    }
+  }
+
+  getAverageRating() {
+    if (this.ratings.length === 0) {
+      return `this is not rated`;
+    }
+    const sum = this.ratings.reduce((acc, curr) => {
+      return acc + curr;
+    }, 0);
+    return Math.round(sum / this.ratings.length);
+  }
+}
+
+class Album extends Media {
+  constructor(title, yearOfRelease, artist, tracks) {
+    super(title, yearOfRelease);
+    this.artist = artist;
+    this.tracks = tracks;
+  }
+}
+
+class Movie extends Media {
+  constructor(title, yearOfRelease, director, runTime) {
+    super(title, yearOfRelease);
+    this.director = director;
+    this.runTime = runTime;
+  }
+}
+
+class Book extends Media {
+  constructor(title, yearOfRelease, author, pages) {
+    super(title, yearOfRelease);
+    this.author = author;
+    this.pages = pages;
+  }
+}
