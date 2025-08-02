b302c26d7d842b5839b37ce8502bad3f4e27edcd
im-mahesh1 <maheshchaudhary2057@gmail.com>
2025-08-02 08:51:14 +0545

    feat: multiple maps is added

--- tests/7-testfirst-review/basics/05-multiple-maps/multiple-maps.js
+++ tests/7-testfirst-review/basics/05-multiple-maps/multiple-maps.js
@@ -1,1 +1,16 @@
 /* eslint-disable no-unused-vars */
+
+function multipleMaps(array1, array2) {
+  debugger;
+  return array1.map((ele) => {
+    debugger;
+    for (let i = 0; i < array2.length; i++) {
+      debugger;
+      if (ele in array2[i]) {
+        return array2[i][ele];
+      }
+    }
+  });
+}
+
+multipleMaps(["a", "b", "c", "d"], [{ a: 0, b: 1 }, { c: 2 }, { d: 3 }]);
