7a3ec105266473ae2b6ffc60a967a8d331c55a0b
im-mahesh1 <maheshchaudhary2057@gmail.com>
2025-08-02 09:23:05 +0545

    feat: make iterator function is added

--- tests/7-testfirst-review/closure/02-make-iterator/make-iterator.js
+++ tests/7-testfirst-review/closure/02-make-iterator/make-iterator.js
@@ -1,1 +1,24 @@
 /* eslint-disable no-unused-vars */
+
+function makeIterator(array = []) {
+  let count = 0;
+  return {
+    getNext: function () {
+      if (count >= array.length) {
+        return {
+          value: undefined,
+          done: true,
+        };
+      }
+      const val = array[count];
+      count++;
+      return {
+        value: val,
+        done: false,
+      };
+    },
+    getIndex: function () {
+      return count;
+    },
+  };
+}
