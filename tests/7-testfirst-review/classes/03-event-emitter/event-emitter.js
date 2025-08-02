bb3331ebf30ad38197cf1b012d7dad9f5571eb18
im-mahesh1 <maheshchaudhary2057@gmail.com>
2025-08-02 09:18:29 +0545

    feat event emitter is added

--- tests/7-testfirst-review/classes/03-event-emitter/event-emitter.js
+++ tests/7-testfirst-review/classes/03-event-emitter/event-emitter.js
@@ -1,1 +1,21 @@
 /* eslint-disable no-unused-vars */
+class MyEventEmitter {
+  constructor() {
+    this.events = {};
+  }
+
+  addListener(eventName, callback) {
+    if (!this.events[eventName]) {
+      this.events[eventName] = [];
+    }
+    this.events[eventName].push(callback);
+  }
+
+  emit(eventName, ...args) {
+    if (this.events[eventName]) {
+      this.events[eventName].forEach((callback) => {
+        callback(...args);
+      });
+    }
+  }
+}
