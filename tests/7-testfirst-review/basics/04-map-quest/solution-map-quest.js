cfd7f2406c4a9c7ee2a40608924fa6a1a263b742
im-mahesh1 <maheshchaudhary2057@gmail.com>
2025-08-02 08:50:46 +0545

    feat: map quest is added

--- tests/7-testfirst-review/basics/04-map-quest/solution-map-quest.js
+++ tests/7-testfirst-review/basics/04-map-quest/solution-map-quest.js
@@ -1,1 +1,17 @@
 /* eslint-disable no-unused-vars */
+function mapQuest(arr) {
+  let map = {
+    N: "S",
+    S: "N",
+    E: "W",
+    W: "E",
+  };
+  return arr.reduce((dir, curDir) => {
+    if (map[curDir] === dir[dir.length - 1]) {
+      dir.pop();
+    } else {
+      dir.push(curDir);
+    }
+    return dir;
+  }, []);
+}
