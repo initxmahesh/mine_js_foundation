f74182d567eea7c852768362904aee6e0aada61d
im-mahesh1 <maheshchaudhary2057@gmail.com>
2025-08-02 09:17:09 +0545

    feat: vehicles is added

--- tests/7-testfirst-review/classes/00-vehicles/vehicle.js
+++ tests/7-testfirst-review/classes/00-vehicles/vehicle.js
@@ -1,1 +1,35 @@
 /* eslint-disable no-unused-vars */
+
+class Vehicle {
+  constructor([wheelTotal, energySource, manufacturer]) {
+    this.wheelTotal = wheelTotal;
+    this.energySource = energySource;
+    this.manufacturer = manufacturer;
+    this.isOn = false;
+  }
+  getSpec(key) {
+    return key === "getSpec" ? undefined : this[key];
+  }
+}
+
+class Tesla extends Vehicle {
+  constructor([
+    milesPerCharge,
+    model,
+    chargePercentage,
+    minutesToCharge,
+    vehicleSpecs,
+  ]) {
+    super(vehicleSpecs[0]);
+    this.milesPerCharge = milesPerCharge;
+    this.model = model;
+    this.chargePercentage = chargePercentage;
+    this.minutesToCharge = minutesToCharge;
+  }
+
+  minutesToFullCharge(currentCharge) {
+    const remainingCharge = 100 - currentCharge;
+    const minutesNeeded = (remainingCharge / 100) * this.minutesToCharge;
+    return `${minutesNeeded} minutes until the charge is full!`;
+  }
+}
