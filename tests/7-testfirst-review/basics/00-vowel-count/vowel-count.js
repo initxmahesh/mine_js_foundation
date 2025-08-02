9939f89531f915cdf8c64221758fa9e24fb13c0b
im-mahesh1 <maheshchaudhary2057@gmail.com>
2025-08-02 08:48:32 +0545

    feat: vowel count is added

--- tests/7-testfirst-review/basics/00-vowel-count/vowel-count.js
+++ tests/7-testfirst-review/basics/00-vowel-count/vowel-count.js
@@ -1,1 +1,50 @@
 /* eslint-disable no-unused-vars */
+function vowelsCount(str) {
+    let lowerCap = str.toLowerCase();
+    let map = {
+        a: 0,
+        e: 0,
+        i: 0,
+        o: 0,
+        u: 0,
+        total: 0
+    }
+    for (let char of lowerCap) {
+        if (char === 'a') map.a++
+        if (char === 'e') map.e++
+        if (char === 'i') map.i++
+        if (char === 'o') map.o++
+        if (char === 'u') map.u++
+    }
+    let vals = Object.values(map)
+    map.total = vals.reduce((acc, curr) => {
+        return acc + curr
+    }, 0)
+    return map;
+}
+
+
+// function vowelsCount(str) {
+//     let strlower = str.toLowerCase();
+//     let map = {
+//         a: 0,
+//         e: 0,
+//         i: 0,
+//         o: 0,
+//         u:0
+//     }
+//     for (let char of str) {
+//         if (char === 'a') map.a++;
+//         if (char === "e") map.e++;
+//         if (char === "i") map.i++;
+//          if (char === "o") map.o++;
+//           if (char === "u") map.u++;
+//     }
+
+//     let values = Object.values(map);
+//     map.total = values.reduce(())
+
+
+
+
+// }
