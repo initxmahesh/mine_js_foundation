71b596c476edfe9ad047a453c3ef020eab8f9cf5
im-mahesh1 <maheshchaudhary2057@gmail.com>
2025-08-02 09:23:54 +0545

    feat: rotater functionality is added

--- tests/7-testfirst-review/closure/03-rotater/rotater.js
+++ tests/7-testfirst-review/closure/03-rotater/rotater.js
@@ -1,1 +1,18 @@
 /* eslint-disable no-unused-vars */
+function rotater(str) {
+  let forward = true;
+  return function (num) {
+    if (num === 0) {
+      return str;
+    }
+    if (num === str.length) {
+      forward = !forward;
+      return str;
+    }
+    if (forward) {
+      return str.slice(num) + str.slice(0, num);
+    } else {
+      return str.slice(-num) + str.slice(0, -num);
+    }
+  };
+}
