ca1d1d37e22e474ab3e0c94fd08e01d1c74c430f
im-mahesh1 <maheshchaudhary2057@gmail.com>
2025-08-02 09:17:40 +0545

    feat: 01 array prototype is added

--- tests/7-testfirst-review/classes/01-array-prototype/array-prototype.js
+++ tests/7-testfirst-review/classes/01-array-prototype/array-prototype.js
@@ -1,1 +1,32 @@
 /* eslint-disable no-unused-vars */
+// Implementing maap (map)
+Array.prototype.maap = function (callback) {
+  const result = [];
+  this.forEach((element, index, array) => {
+    result.push(callback(element, index, array));
+  });
+  return result;
+};
+
+// Implementing fiilter (filter)
+Array.prototype.fiilter = function (callback) {
+  const result = [];
+  this.forEach((element, index, array) => {
+    if (callback(element, index, array)) {
+      result.push(element);
+    }
+  });
+  return result;
+};
+
+// Implementing reeduce (reduce)
+Array.prototype.reeduce = function (callback, initialValue) {
+  let accumulator = initialValue !== undefined ? initialValue : this[0];
+  let startIndex = initialValue !== undefined ? 0 : 1;
+
+  this.slice(startIndex).forEach((currentValue) => {
+    accumulator = callback(accumulator, currentValue);
+  });
+
+  return accumulator;
+};
