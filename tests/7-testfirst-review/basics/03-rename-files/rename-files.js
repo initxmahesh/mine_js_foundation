3b2c41039682f98e484cf7cb999838cd94eed8d0
im-mahesh1 <maheshchaudhary2057@gmail.com>
2025-08-02 08:50:14 +0545

    feat: rename files is added

--- tests/7-testfirst-review/basics/03-rename-files/rename-files.js
+++ tests/7-testfirst-review/basics/03-rename-files/rename-files.js
@@ -1,1 +1,25 @@
 /* eslint-disable no-unused-vars */
+function renameFiles(file) {
+  let result = [];
+  let map = {};
+
+  for (let ele of file) {
+    if (!map[ele]) {
+      result.push(ele);
+      map[ele] = 1;
+    } else {
+      let rname = `${ele}(${map[ele]})`;
+
+      while (map[rname]) {
+        map[ele]++;
+        rname = `${ele}(${map[ele]})`;
+      }
+
+      result.push(rname);
+      map[rname] = 1;
+      map[ele]++;
+    }
+  }
+
+  return result;
+}
