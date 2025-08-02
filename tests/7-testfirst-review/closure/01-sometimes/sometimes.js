87e9b341240391bf54826dd656d393b84066e052
im-mahesh1 <maheshchaudhary2057@gmail.com>
2025-08-02 09:22:25 +0545

    feat: sometimes function is added

--- tests/7-testfirst-review/closure/01-sometimes/sometimes.js
+++ tests/7-testfirst-review/closure/01-sometimes/sometimes.js
@@ -1,1 +1,18 @@
 /* eslint-disable no-unused-vars */
+
+function sometimes(func) {
+  let count = 0;
+  return function (...args) {
+    count++;
+    if (count <= 3) {
+      return func(...args);
+    }
+    if (count > 3) {
+      if (count % 2 === 1) {
+        return func(...args);
+      } else {
+        return `I do not know!`;
+      }
+    }
+  };
+}
