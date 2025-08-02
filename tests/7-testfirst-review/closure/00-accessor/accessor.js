843b8c92bc360dfd8a0252957fb12060a787a911
im-mahesh1 <maheshchaudhary2057@gmail.com>
2025-08-02 09:21:56 +0545

    feat: accessor is added

--- tests/7-testfirst-review/closure/00-accessor/accessor.js
+++ tests/7-testfirst-review/closure/00-accessor/accessor.js
@@ -1,1 +1,9 @@
 /* eslint-disable no-unused-vars */
+function accessor(obj) {
+  return function (key, value) {
+    if (arguments.length === 2) {
+      obj[key] = value;
+    }
+    return obj[key];
+  };
+}
