710716eb573f8c9d8ca0faf05a03de8e500a7d08
im-mahesh1 <maheshchaudhary2057@gmail.com>
2025-08-03 09:08:22 +0545

    feat: donot include functionality is added

--- tests/7-testfirst-review/this-keyword-context/01-do-not-include/do-not-include.js
+++ tests/7-testfirst-review/this-keyword-context/01-do-not-include/do-not-include.js
@@ -1,1 +1,5 @@
 /* eslint-disable no-unused-vars, no-extend-native */
+Array.prototype.doNotInclude = function (indices) {
+  const excludeIndices = Array.isArray(indices) ? indices : [indices];
+  return this.filter((_, index) => !excludeIndices.includes(index));
+};
