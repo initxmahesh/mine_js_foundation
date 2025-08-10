ea421efdd2f77426528097f379693f81233eaf98
im-mahesh1 <maheshchaudhary2057@gmail.com>
2025-08-10 22:20:31 +0545

    feat: keypad is added

--- tests/4-checkpoint-pt-2/04_keypad/solution_keypad.js
+++ tests/4-checkpoint-pt-2/04_keypad/solution_keypad.js
@@ -1,1 +1,43 @@
-/* eslint-disable no-unused-vars */
+function presses(str) {
+  let obj = {
+    2: "ABC",
+    3: "DEF",
+    4: "GHI",
+    5: "JKL",
+    6: "MNO",
+    7: "PQRS",
+    8: "TUV",
+    9: "WXYZ",
+    10: "1",
+    11: "2",
+    12: "3",
+    13: "4",
+    14: "5",
+    15: "6",
+    16: "7",
+    17: "8",
+    18: "9",
+    19: "0",
+  };
+  let sum = 0;
+  let caps = str.toUpperCase();
+  if (caps.length === 1) {
+    for (let key in obj) {
+      if (obj[key].includes(caps)) {
+        return obj[key].indexOf(caps) + 1;
+      }
+    }
+  }
+
+  for (let ele of caps) {
+    if (ele === " ") {
+      sum += 1;
+    }
+    for (key in obj) {
+      if (obj[key].includes(ele)) {
+        sum += obj[key].indexOf(ele) + 1;
+      }
+    }
+  }
+  return sum;
+}
