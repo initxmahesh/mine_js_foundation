8bb91d06f53144690d74e2f800310a30661e092b
im-mahesh1 <maheshchaudhary2057@gmail.com>
2025-08-10 22:08:46 +0545

    feat: vending machine is added

--- tests/8-checkpoint-final/02_vending-machine/solution-vending-machine.js
+++ tests/8-checkpoint-final/02_vending-machine/solution-vending-machine.js
@@ -1,1 +1,21 @@
-/* eslint-disable no-unused-vars */
+class VM {
+  constructor(inventory) {
+    this.inventory = inventory;
+  }
+
+  sale(itemID) {
+    let item = this.inventory[itemID];
+    if (item && item.stock > 0) {
+      item.stock--;
+      return `1 ${item.name} - Thank you and come again!`;
+    }
+  }
+
+  stockTotal() {
+    const totalStock = Object.values(this.inventory)
+      .map((item) => item.stock)
+      .reduce((sum, stock) => sum + stock, 0);
+
+    return totalStock > 0 ? `${totalStock} item(s)` : "Out of Stock";
+  }
+}
