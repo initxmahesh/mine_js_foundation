42b72dcdcec5375065dd1f6339c0977176b7734b
im-mahesh1 <maheshchaudhary2057@gmail.com>
2025-08-10 22:18:21 +0545

    feat: array methods is added

--- tests/4-checkpoint-pt-2/02_array-methods/solutions_array_methods.js
+++ tests/4-checkpoint-pt-2/02_array-methods/solutions_array_methods.js
@@ -1,1 +1,31 @@
-/* eslint-disable no-unused-vars */
+function useMapToUpperCase(str) {
+  return str.split(" ").map((el) => {
+    return el.toUpperCase();
+  });
+}
+
+function useFilter(arr) {
+  return arr.filter((ele) => {
+    if (ele.includes("@")) return ele;
+  });
+}
+
+function sumWithReduce(arr, start = 0) {
+  return arr.reduce((acc, ele) => {
+    acc += ele;
+    return acc;
+  }, start);
+}
+
+function filterEvensDoubleAndSum(arr) {
+  return arr
+    .filter((el) => {
+      if (el % 2 === 0) return true;
+    })
+    .map((el) => {
+      return el * 2;
+    })
+    .reduce((acc, el) => {
+      return (acc += el);
+    }, 0);
+}
