d141fa1f05e5d82abd939d911f3da073a85b1ac6
im-mahesh1 <maheshchaudhary2057@gmail.com>
2025-08-10 22:10:56 +0545

    feat: key adder is added

--- tests/8-checkpoint-final/04_key_adder/solution-key-adder.js
+++ tests/8-checkpoint-final/04_key_adder/solution-key-adder.js
@@ -1,1 +1,9 @@
-/* eslint-disable no-unused-vars, no-prototype-builtins */
+function keyAdder() {
+  let sum = 0;
+  for (let key in this) {
+    if (this.hasOwnProperty(key) && typeof this[key] === "number") {
+      sum += this[key];
+    }
+  }
+  return sum;
+}
