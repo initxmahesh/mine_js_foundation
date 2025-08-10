f5391783ab74e32411ab4d24e43978d58bbf7d94
im-mahesh1 <maheshchaudhary2057@gmail.com>
2025-08-10 22:07:08 +0545

    feat alternate and twice is added

--- tests/8-checkpoint-final/01_alternate_and_twice/solution-alternate-and-twice.js
+++ tests/8-checkpoint-final/01_alternate_and_twice/solution-alternate-and-twice.js
@@ -1,1 +1,19 @@
-/* eslint-disable no-unused-vars, no-prototype-builtins */
+function alternate(func) {
+  let count = 0;
+  return function () {
+    count++;
+    if (count % 2 === 1) {
+      return func();
+    } else return 0;
+  };
+}
+
+function twice(func) {
+  let count = 0;
+  return function () {
+    if (count < 2) {
+      count++;
+      return func();
+    } else return 0;
+  };
+}
