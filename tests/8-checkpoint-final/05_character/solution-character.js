5226694e610447e9d68169623277140dc07df731
im-mahesh1 <maheshchaudhary2057@gmail.com>
2025-08-10 22:12:37 +0545

    feat: character is added to the sys

--- tests/8-checkpoint-final/05_character/solution-character.js
+++ tests/8-checkpoint-final/05_character/solution-character.js
@@ -1,1 +1,47 @@
-/* eslint-disable no-unused-vars, no-prototype-builtins */
+class Character {
+  constructor(character, fontName, fontSize) {
+    this.character = character;
+    this.fontName = fontName;
+    this.fontSize = fontSize;
+  }
+
+  drawCharacter() {
+    return this.character;
+  }
+}
+
+class Letter extends Character {
+  constructor(character, fontName, fontSize, charCase) {
+    super(character, fontName, fontSize);
+    this.charCase = charCase;
+  }
+
+  toLowerCase() {
+    this.charCase = "lowerCase";
+    this.character = this.character.toLowerCase();
+  }
+
+  toUpperCase() {
+    this.charCase = "upperCase";
+    this.character = this.character.toUpperCase();
+  }
+
+  getCase() {
+    return this.charCase;
+  }
+}
+
+class Digit extends Character {
+  constructor(character, fontName, fontSize, isDigit = false) {
+    super(character, fontName, fontSize);
+    if (typeof this.character === "number") {
+      this.isDigit = true;
+    } else {
+      this.isDigit = isDigit;
+    }
+  }
+  integerValue() {
+    this.character = parseInt(this.character);
+    this.isDigit = true;
+  }
+}
